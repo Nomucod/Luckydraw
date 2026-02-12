529c2e5a7fe4327e3969558384dd6ed8923761ac
cyclonenkfust <cyclonenkfust@gmail.com>
2026-02-12 18:15:52 +0800

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("找不到 root 節點，請檢查 index.html 是否正確");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
